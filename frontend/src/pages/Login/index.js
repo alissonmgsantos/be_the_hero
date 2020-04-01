964831ccf36e717d5520386d63a2e1b4b00ad447
Alisson Matos <alissonmgsantos@gmail.com>
2020-04-01 07:16:34 -0300

    Finalização da interface web do projeto

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
 
 const Login = () => {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (err) {
+      console.log(err);
+      alert('Falha no login, tente novamente.');
+    }
+  }
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Acessar</h1>
-          <input type="text" placeholder="Sua identificação" />
+          <input
+            type="text"
+            placeholder="Sua identificação"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button type="submit" className="button">
             Acessar
           </button>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
         <Route path="/register" exact component={Register} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/incidents/new" exact component={NewIncident} />
       </Switch>
     </BrowserRouter>
   );
